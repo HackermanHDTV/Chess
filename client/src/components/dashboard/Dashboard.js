cc8856d8483489171e9e3837fcc087cf668418e8
Vivek <vivek.ranjaan@gmail.com>
2021-06-13 19:03:19 +0530

    fixed dashboard bug

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,9 +1,9 @@
-import React from "react";
-import { useUser } from "../contexts/UserContext";
-import { Link } from "react-router-dom";
+import React from 'react'
+import { useUser } from '../contexts/UserContext'
+import { Link } from 'react-router-dom'
 
 export default function Dashboard() {
-  const { user } = useUser();
+  const { user } = useUser()
   return (
     <div className='dashboard'>
       <div>
@@ -13,9 +13,11 @@ export default function Dashboard() {
           <p>You are not Logged in</p>
         )}
       </div>
-      <div className='prompt'>
-        Play a game? <Link to='/play-online'>Play Online</Link>
-      </div>
+      {user && (
+        <div className='prompt'>
+          Play a game? <Link to='/play-online'>Play Online</Link>
+        </div>
+      )}
     </div>
-  );
+  )
 }
