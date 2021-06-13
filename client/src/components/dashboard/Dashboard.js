94f5df58975558f079a4a292497111e49c9f08f8
Tintintani <tanishqarora02@gmail.com>
2021-06-13 18:55:17 +0530

    completed dashboard

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,5 +1,21 @@
-import React from 'react'
+import React from "react";
+import { useUser } from "../contexts/UserContext";
+import { Link } from "react-router-dom";
 
 export default function Dashboard() {
-  return <>Dashboard</>
+  const { user } = useUser();
+  return (
+    <div className='dashboard'>
+      <div>
+        {user ? (
+          <p> {`Hi, ${user.username}`}! </p>
+        ) : (
+          <p>You are not Logged in</p>
+        )}
+      </div>
+      <div className='prompt'>
+        Play a game? <Link to='/login'>Play Online</Link>
+      </div>
+    </div>
+  );
 }
