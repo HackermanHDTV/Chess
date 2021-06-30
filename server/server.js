c51d3f1242ff237c89b6e42c436511a7d3a3949d
Dinesh Chukkala <dineshchukkala169@gmail.com>
2021-06-30 13:02:15 +0530

    hosting

--- server/server.js
+++ server/server.js
@@ -2,15 +2,15 @@ const express = require('express')
 const cors = require('cors')
 const http = require('http')
 require('dotenv').config()
-const port = 5000
+const port = process.env.PORT | 5000
 const mongoose = require('mongoose')
 const app = express()
 const server = http.createServer(app)
 const { Server } = require('socket.io')
 const io = new Server(server, {
-	cors: {
-		origin: '*',
-	},
+  cors: {
+    origin: '*',
+  },
 })
 
 const { v4 } = require('uuid')
@@ -19,51 +19,51 @@ const { userSignUp, userLogin, getUser } = require('./util/AuthRoutes.js')
 const { getPGN, savePGN } = require('./util/PGNroute.js')
 
 mongoose.connect(
-	process.env.MONGODB_SRV,
-	{
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-	},
-	() => {
-		console.log('Mongoose connected!')
-	}
+  process.env.MONGODB_SRV,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  () => {
+    console.log('Mongoose connected!')
+  }
 )
 
 let availableRoom
 
 io.on('connection', (socket) => {
-	console.log('socket connected!')
+  console.log('socket connected!')
 
-	socket.on('join-room', () => {
-		if (availableRoom) {
-			socket.join(availableRoom)
+  socket.on('join-room', () => {
+    if (availableRoom) {
+      socket.join(availableRoom)
 
-			io.to(socket.id).emit('init', availableRoom, 'b')
-			io.to(availableRoom).emit('begin')
+      io.to(socket.id).emit('init', availableRoom, 'b')
+      io.to(availableRoom).emit('begin')
 
-			availableRoom = null
-		} else {
-			availableRoom = v4()
+      availableRoom = null
+    } else {
+      availableRoom = v4()
 
-			socket.join(availableRoom)
+      socket.join(availableRoom)
 
-			io.to(socket.id).emit('init', availableRoom, 'w')
-		}
-	})
+      io.to(socket.id).emit('init', availableRoom, 'w')
+    }
+  })
 
-	socket.on('move', (roomID, moveObj) => {
-		socket.broadcast.to(roomID).emit('make-move', moveObj)
-	})
+  socket.on('move', (roomID, moveObj) => {
+    socket.broadcast.to(roomID).emit('make-move', moveObj)
+  })
 })
 
 // middleware
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(
-	cors({
-		origin: '*',
-		credentials: true,
-	})
+  cors({
+    origin: '*',
+    credentials: true,
+  })
 )
 
 // Auth routes
@@ -76,5 +76,5 @@ app.get('/api/getPGN/:id', getPGN)
 app.post('/api/postPGN', savePGN)
 
 server.listen(port, '0.0.0.0', () => {
-	console.log(`Server is listening on port ${port}`)
+  console.log(`Server is listening on port ${port}`)
 })
