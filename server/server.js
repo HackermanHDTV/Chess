9ae7983af98b6a0852488b2a9708e63490c254a6
Dinesh Chukkala <dineshchukkala169@gmail.com>
2021-07-03 12:55:35 +0530

    hosting

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const server = http.createServer(app)
 const { Server } = require('socket.io')
 const io = new Server(server, {
   cors: {
-    origin: '*',
+    origin: process.env.CLIENT_API,
   },
 })
 
